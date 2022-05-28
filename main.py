c4ce32e48ae7d8393922fde9b03338ca573468b8
benson890924 <71247954+benson890924@users.noreply.github.com>
2022-05-29 05:36:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from load import load_image
-from model import build_model, apply_model
+from model import simple_cnn
 
-from skimage.color import lab2rgb, rgb2gray
-from skimage.io import imsave
+from skimage.transform import resize
+from skimage.color import rgb2lab, lab2rgb, rgb2gray
+from skimage.io import imread, imsave
+from keras.models import load_model
 import numpy as np
 import pickle
 import glob
@@ -19,16 +21,23 @@ if not os.path.exists(data_path):
 
 with open(data_path, 'rb') as f:
     data = pickle.load(f)
-img_lab = data[np.random.randint(0, len(data))]
+    img_lab = np.asarray(data)  # adjust the training data size
+    L = img_lab[:, :, :, 0]
+    ab = img_lab[:, :, :, 1:]/128
+    L = L.reshape(len(img_lab), 256, 256, 1)
+    ab = ab.reshape(len(img_lab), 256, 256, 2)
+    m = simple_cnn()
+    m.fit(x=L, y=ab, batch_size=32, epochs=3)
+    m.save('Table/simple_model.h5')
+
+m = load_model('Table/simple_model.h5')
+img = imread('test.png')
+img_lab = rgb2lab(resize(img[:, :, :3], (256, 256)))
 L = img_lab[:, :, 0]
-ab = img_lab[:, :, 1:]/128
-L = L.reshape(1, 400, 400, 1)
-ab = ab.reshape(1, 400, 400, 2)
-
-m = build_model()
-output = apply_model(m, L, ab, epoch=500)
-
-color_result = np.zeros((400, 400, 3))
+L = L.reshape(1, 256, 256, 1)
+output = m.predict(L)
+output *= 128
+color_result = np.zeros((256, 256, 3))
 color_result[:, :, 0] = L[0][:, :, 0]
 color_result[:, :, 1:] = output[0]
 imsave("grayscale.png", rgb2gray(lab2rgb(color_result)))
