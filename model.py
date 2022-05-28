9bbf5c8400d3b4a9db5dd9b17a792ae791e63921
benson890924 <71247954+benson890924@users.noreply.github.com>
2022-05-29 05:18:03 +0800

    Update model.py

--- model.py
+++ model.py
@@ -2,27 +2,23 @@ from keras.layers import Conv2D, UpSampling2D, InputLayer
 from keras.models import Sequential
 
 
-def build_model():
+def simple_cnn():
     model = Sequential()
-    model.add(InputLayer(input_shape=(None, None, 1)))
+    model.add(InputLayer(input_shape=(256, 256, 1)))
     model.add(Conv2D(8, (3, 3), activation='relu', padding='same', strides=2))
     model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
     model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
     model.add(Conv2D(16, (3, 3), activation='relu', padding='same', strides=2))
     model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
     model.add(Conv2D(32, (3, 3), activation='relu', padding='same', strides=2))
-    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
     model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-    model.add(UpSampling2D((2, 2)))
     model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
     model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(4, (3, 3), activation='relu', padding='same'))
     model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
+    model.add(UpSampling2D((2, 2)))
+    model.compile(optimizer='adam', loss="mse")
     return model
-
-
-def apply_model(model, L, ab, epoch=1000):
-    model.compile(optimizer='rmsprop', loss='mse')
-    model.fit(x=L, y=ab, batch_size=1, epochs=epoch)
-    output = model.predict(L)
-    output *= 128
-    return output
